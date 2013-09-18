255a6b95e9c0a1cb64ef833a7ae3eff542478e2e
DBC Apprentice <apprentice@dbc.com>
2013-09-18 15:59:46 -0700

    user has username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,32 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
-  has_secure_password
   validates_presence_of :username, :password_digest
+  validates :username, uniqueness: true
 
-  # attr_accessible :username, :password_digest
-  # has_many :responses, as: :responseable
+  attr_accessible :username, :password_digest
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def ask_question(title, content)
+    raise ArgumentError if title == nil || content == nil
+    self.questions << Question.create(title: title, content: content)
+  end
+  
+end
+
+class ArgumentError < StandardError; end
+
+
+
+# has_many :responses, as: :responseable
   # has_many :questions
   # has_many :votes, :source => :votes, as: :voteable
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,40 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.new(username: "test", password: "12345678") }
+  let(:bpassword) { BCrypt::Password.create("my password") }
+  let(:user) { User.create(username: "test", password_digest: bpassword) }
   describe "#initialize" do
 
-    it "does not initialize with invalid password" do
-      User.new(username: "foo", password: "123")
-    end
-
-    it "does not initialize when username is not unique" do
-      user.save
-      expect { user.create(username: "test") }.to 
+    it "does not save to DB when username is not unique" do
+      expect { User.create(username: "test") }.to_not change{User.count}
     end
 
     it "is able to persist in the database when saved" do
-      pending
-      # expect { User.create }.to change { User.all.count }.by(1)
+      expect { user }.to change { User.count }.by(1)
     end
 
-    it "save bcrypt password in the DB" do
-      pending
+    it "raises error when either username or password is missing" do
+      expect { User.create(username: "foo") }.to_not change{User.count}
+      expect { User.create }.to_not change{User.count}
+
     end
+
+    # it "save bcrypt password in the DB" do
+    #   expect(user.password_digest).to_not eq(User.find_by_username("test").password_digest)
+    # end
+  end
+
+  describe "#ask_question" do
+    it "create question in DB with title and content that associate with the user" do
+      pending
+      # expect {user.ask_question("this is title", "this is content") }.to change{ user.questions}.by(1)
+    end
+
+    it "errs when title or content is nil" do
+      expect { user.ask_question }.to raise_error(ArgumentError)
+    end
+
+
   end
 
 end
