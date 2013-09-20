26c476f23fc9ad8f590aeb78c755fe616979c138
dbcapprentice <dbcapprentice@dbc.com>
2013-09-19 17:46:39 -0700

    full suite of tests for questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,4 @@
-class QuestionsController < ActionController::Base
+class QuestionsController < ApplicationController
 
   def index
   	@questions = Question.all
@@ -18,13 +18,21 @@ class QuestionsController < ActionController::Base
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
-
   def update
+    @question = Question.find(params[:id])
+    # @question.title = params[:question][:title]
+    # @question.content = params[:question][:content]
+    # @question.save
+    # @question.update_attributes(params[:question])
+    redirect_to @question
   end
 
   def destroy
+    @question = Question.find(params[:id]).destroy
+    redirect_to root_path
   end
 
 end
