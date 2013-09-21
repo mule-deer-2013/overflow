73825a0794ef4e8f598759e34013140109296d7f
Dev Bootcamp <Apprentice@dbc35.local>
2013-09-20 17:04:50 -0700

    edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :responses
-  has_many :votes
+  has_many :votes, as: :votable
 
   validates_presence_of :username
   validates :username, uniqueness: true
