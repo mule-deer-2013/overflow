d8397ce3fee9d31aabcfbb982f1952558198d9fc
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-09-24 10:49:09 -0700

    fixed response creation

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -10,14 +10,12 @@ def new
 end
 
 def create
-  @response = Response.new(body: params[:response][:body], question_id: params[:question_id])
-  question = Question.find(params[:question_id])
-  if @response.save
-    render :json => render_to_string(:partial => 'responses/responses', :locals => {:response => @response }).to_json
-
+  @question = Question.find(params[:question_id])
+  response = @question.responses.new(params[:response])
+  if response.save
+    render :json => render_to_string(:partial => 'responses/responses', :locals => {:response => response }).to_json
   else
-    @errors = @response.errors.full_messages
-    render :json => render_to_string(:partial => 'responses/error', :locals => {:errors => @errors }).to_json, status: :unprocessable_entity 
+    render :json => render_to_string(:partial => 'responses/error', :locals => {:errors => response.errors.full_messages.join(',') }).to_json, status: :unprocessable_entity 
   end
 end
 
