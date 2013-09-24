674ab5466ce4d15c98552427788f64323a84af86
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-09-24 10:33:49 -0700

    fix broken spec

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 
 group :development, :test do
   gem 'rspec-rails'
-  gem 'factory_girl'
+  gem 'factory_girl_rails'
   gem 'capybara'
   gem "shoulda-matchers"
   gem "launchy"
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,9 @@
-Factory.define :user do |user|
-	user.username "Ali Tehrani"
-	user.email "ali@tehrani.eu"
-	user.password "foobar"
+require 'factory_girl'
+
+FactoryGirl.define do
+  factory :user do |user|
+  	user.username "Ali Tehrani"
+  	user.email "ali@tehrani.eu"
+  	user.password "foobar"
+  end
 end
